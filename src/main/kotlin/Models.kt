316812fa9cdbded8b733c6757e2e84b083b12f6d
Jordan Pitlor <jordan@pitlor.dev>
2021-02-04 10:10:52 -0500

    add socket io

--- src/main/kotlin/Models.kt
+++ src/main/kotlin/Models.kt
@@ -1,4 +1,7 @@
-class Player(val name: String) {
+import java.util.*
+import kotlin.collections.ArrayList
+
+class Player(val name: String, val uuid: UUID) {
     var score: Int = 0
     var puddingCount: Int = 0
     val hand: ArrayList<Card> = arrayListOf()
--- src/main/kotlin/Server.kt
+++ src/main/kotlin/Server.kt
@@ -1,13 +1,34 @@
-import io.socket.engineio.server.EngineIoServer
-import io.socket.engineio.server.EngineIoSocket
+import com.corundumstudio.socketio.Configuration
+import com.corundumstudio.socketio.SocketIOServer
+import java.util.*
 
-fun main() {
-    val server = EngineIoServer()
-    server.on("connection") { connection ->
-        val socket = connection[0] as EngineIoSocket
+enum class Event {
 
-        socket.on("message") { message ->
+}
 
+class Server(private val onMessage: Map<String, (UUID, List<Any>) -> Unit>) {
+    private val server: SocketIOServer
+
+    init {
+        val config = Configuration()
+        config.hostname = System.getProperty("host") ?: "localhost"
+        config.port = System.getProperty("port").toIntOrNull() ?: 8080
+
+        server = SocketIOServer(config)
+    }
+
+    fun start() {
+        server.start()
+        server.addEventInterceptor { client, event, args, _ ->
+            onMessage[event]?.let { it(client.sessionId, args) }
+        }
+    }
+
+    fun sendMessage(event: Event, message: Any, game: String, player: Player?) {
+        if (player == null) {
+            server.getRoomOperations(game).sendEvent(event.name, message)
+        } else {
+            server.getClient(player.uuid).sendEvent(event.name, message)
         }
     }
 }

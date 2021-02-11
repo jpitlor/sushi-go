44c00b45e901ce77994228f4ad5b9fe629b522f1
Jordan Pitlor <jordan@pitlor.dev>
2021-02-11 15:58:43 -0500

    made a lobby page

--- pages/Lobby.tsx
+++ pages/Lobby.tsx
@@ -1,28 +1,86 @@
-import { Box, Button, Input } from "@chakra-ui/react";
-import React from "react";
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Heading,
+  Image,
+  Input,
+  Select,
+  useRadioGroup,
+  VStack,
+} from "@chakra-ui/react";
+import { getGlobalLock } from "framer-motion/types/gestures/drag/utils/lock";
+import React, { FormEvent } from "react";
 import { useDispatch } from "react-redux";
+import RadioCard from "../components/RadioCard";
 import { createGame, useSelector } from "../data/store";
+import logo from "../public/logo.png";
+import skins from "../skins";
 import useInput from "../utils/useInput";
 
 export default function Lobby() {
-  const [gameCode, setGameCode] = useInput("");
+  const [newGameCode, setNewGameCode] = useInput("");
   const dispatch = useDispatch();
   const openGames = useSelector((state) => state.openGames);
 
-  function handleCreateGame() {
-    dispatch(createGame(gameCode));
-    setGameCode("");
+  const { getRootProps, getRadioProps, value: gameCode } = useRadioGroup({
+    name: "game-code",
+    defaultValue: "",
+  });
+  const selectRootProps = getRootProps();
+
+  function handleJoinGame(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if (gameCode === "Create Game") {
+      dispatch(createGame(newGameCode));
+    }
+
+    return false;
   }
 
   return (
-    <Box p={4}>
-      <Input value={gameCode} onChange={setGameCode} placeholder="Game Code" />
-      <Button onClick={handleCreateGame}>Create Game</Button>
-      <ul>
-        {openGames.map((g) => (
-          <li key={g}>{g}</li>
-        ))}
-      </ul>
+    <Box background="red.300" w="100vw" h="100vh" py={16}>
+      <Flex
+        mx="auto"
+        flexDirection="column"
+        alignItems="center"
+        maxW="40vw"
+        background="white"
+        boxShadow="md"
+        borderRadius="md"
+        p={8}
+      >
+        <Image src={logo} mb={8} />
+
+        <form onSubmit={handleJoinGame}>
+          <Heading mb={4}>Open Games</Heading>
+          <VStack {...selectRootProps} spacing={4}>
+            {openGames.map((value) => (
+              <RadioCard key={value} {...getRadioProps({ value })}>
+                {value}
+              </RadioCard>
+            ))}
+            <RadioCard {...getRadioProps({ value: "Create Game" })}>
+              Create Game
+            </RadioCard>
+            {gameCode === "Create Game" && (
+              <FormControl id="game-code">
+                <FormLabel mt={4}>Game Code</FormLabel>
+                <Input
+                  value={newGameCode}
+                  onChange={setNewGameCode}
+                  placeholder="Game Code"
+                />
+              </FormControl>
+            )}
+            <Button type="submit">Join Game</Button>
+          </VStack>
+        </form>
+      </Flex>
     </Box>
   );
 }
